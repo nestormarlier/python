4c1707adba54de0fbb33a542618ff344f9ccaf98
Nestor <nestor.mar@gmail.com>
2019-09-20 23:02:44 -0300

    nuevos bucles

--- while.py
+++ while.py
@@ -1,21 +1,18 @@
 import math
 
-numero=int(input("Ingrese el numero que desea averiguar su raiz cuadrada: "))
+print("Calculo raiz cuadrada")
+num=int(input("Ingresar el numero que desa averiguar las raiz cuadrada "))
 
 intentos=0
 
-while numero<0:
-    print("No existe la raiz cuadrada de un numero negativo")
+while num<0:
+    print("No se puede obtener la raiz cuadrad de un numero negativo")
     
-    if intentos==2:
-        print("Ha tenido demasiados intentos")
-        break
-    
-    numero=int(input("Ingrese el numero que desea averiguar su raiz cuadrada: "))
-    if numero<0:
-        intentos=intentos+1
+    if intentos>2:
+        print("Demasiados intentos")
+        break;
+    intentos=intentos + 1
+    num=int(input("ingrese numero nuevamente: "))
 
 if intentos<2:
-    solucion=math.sqrt(numero)
-    print(f"El valor de la raiz cuadrada de {numero} es " + str(solucion))
-    
+    print(f"La raiz cuadrada de {num} es " + str(math.4sqrt(num)))
